3845f63ea40e178d7a3dd5444980f80db0f8adfb
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-23 11:12:39 +0500

    fix: use suspanse

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,10 +1,20 @@
-import EditableTable from '@/components/ui/editable-table/EditableTable';
+import UserDrawer from '@/components/ui/user/drawer/UserDrawer';
+import EditableTable from '@/components/ui/user/editable-table/EditableTable';
+import { Flex, Spin } from 'antd';
 import { FC, Suspense } from 'react';
 
 const Home: FC = () => {
   return (
-    // <Suspense>Home</Suspense>
-    <EditableTable />
+    <Flex className='container' gap='30px' vertical>
+      <Flex justify='space-between' gap='20px' align='center'>
+        <h2 className='heading-2'>User Table</h2>
+        <UserDrawer />
+      </Flex>
+
+      <Suspense key='UserTable' fallback={<Spin size='large' />}>
+        <EditableTable />
+      </Suspense>
+    </Flex>
   );
 };
 
