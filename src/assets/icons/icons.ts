5875a356c24d9374dc3a45f63591203c55e6bde0
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-23 09:34:40 +0500

    feat: add icons

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,4 @@
+export { default as IconTrash } from './trash.svg';
+export { default as IconEdit } from './edit.svg';
+export { default as IconSave } from './save.svg';
+export { default as IconCancle } from './cancle.svg';
