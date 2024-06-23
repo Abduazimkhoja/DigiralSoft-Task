d122a841b51c7e674f55e1e85e2cd08b4c0b3b2f
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-23 09:32:51 +0500

    feat: add next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cloudflare-ipfs.com', pathname: '**' },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
