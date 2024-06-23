0deb5d0a47b60385ca4c553dd1c363cf7418bda5
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-23 12:02:06 +0500

    fix: remove import AntdRegistry

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,6 +1,5 @@
 'use client';
 import StoreProvider from '@/store/StoreProvider';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { FC, ReactNode } from 'react';
 
 interface IProviders {
