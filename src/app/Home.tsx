9ae1d782ee5cd6b8d3ef1a7770c8c3af9c4e53e3
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-23 09:35:37 +0500

    refactor: create home component

---
+++ src/app/Home.tsx
@@ -0,0 +1,11 @@
+import EditableTable from '@/components/ui/editable-table/EditableTable';
+import { FC, Suspense } from 'react';
+
+const Home: FC = () => {
+  return (
+    // <Suspense>Home</Suspense>
+    <EditableTable />
+  );
+};
+
+export default Home;
